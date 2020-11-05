9d3f3c1f11d8cf26560ef9f9b077170d43070a11
AlxMackMoo <alexander.macklin@mds.torrens.edu.au>
2020-11-06 03:35:34 +1000

    Enemy Moving Again
    I got him moving again, he can't go through a door but he can move

---
+++ Assets/Scripts/WaypointController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointController : MonoBehaviour
+{
+
+    public List<Transform> waypoints = new List<Transform>();
+    private Transform targetWaypoint;
+    private int targetWaypointIndex = 0;
+    private float minDistance = 0.1f;
+    private int lastWaypointIndex;
+
+    public float movementSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetWaypoint = waypoints[targetWaypointIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementStep = movementSpeed * Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, movementStep);
+    }
+}
